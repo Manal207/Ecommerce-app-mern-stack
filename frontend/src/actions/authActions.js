25f1c9ba89540519b9deba51ba7a71da9ccd0ee0
Manal Ahmadi <manal.ahmadi@uir.ac.ma>
2025-03-07 13:02:02 +0000

    resume

--- frontend/src/actions/authActions.js
+++ frontend/src/actions/authActions.js
@@ -6,6 +6,7 @@ import {
     REGISTER_SUCCESS,
     REGISTER_FAIL,
     USER_LOGOUT,
+    USER_LOGOT,
     AUTH_ERROR,
   } from '../constants/authConstants';
 
@@ -29,3 +30,27 @@ export const loginUser = (userData) => async (dispatch) => {
         dispatch({ type: LOGIN_FAIL, payload: err.response.data.msg });
     }
 };
+
+
+
+
+
+// // Register User
+// export const registerUser = (userData) => async (dispatch) => {
+//     try {
+//         const res = await axios.post('/api/auth/register', userData);
+//         dispatch({ type: REGISTER_SUCCESS, payload: res.data });
+//     } catch (err) {
+//         dispatch({ type: REGISTER_FAIL, payload: err.response.data.msg });
+//     }
+// };
+
+// // Login User
+// export const loginUser = (userData) => async (dispatch) => {
+//     try {
+//         const res = await axios.post('/api/auth/login', userData);
+//         dispatch({ type: LOGIN_SUCCESS, payload: res.data });
+//     } catch (err) {
+//         dispatch({ type: LOGIN_FAIL, payload: err.response.data.msg });
+//     }
+// };
